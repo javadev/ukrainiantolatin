531f3aa766f010f38c9b9afba0d197c385e97cc7
Valentyn Kolesnikov <valentyn.kolesnikov@kruschecompany.com>
2013-01-17 16:16:19 +0200

    Improved dart version

--- src/main/dart/ukrainiantolatin.dart
+++ src/main/dart/ukrainiantolatin.dart
@@ -57,17 +57,17 @@ class ConvertCase {
 }
 
 class UkrainianToLatin {
-    const int INDEX_0 = 0;
-    const int INDEX_1 = 1;
-    const int INDEX_2 = 2;
-    const int INDEX_3 = 3;
-    const int INDEX_4 = 4;
-    const int INDEX_8 = 8;
-    const int LENGTH_1 = 1;
-    const int LENGTH_2 = 2;
-    const int LENGTH_3 = 3;
-    const int LENGTH_4 = 4;
-    const int LENGTH_8 = 8;
+    static const int INDEX_0 = 0;
+    static const int INDEX_1 = 1;
+    static const int INDEX_2 = 2;
+    static const int INDEX_3 = 3;
+    static const int INDEX_4 = 4;
+    static const int INDEX_8 = 8;
+    static const int LENGTH_1 = 1;
+    static const int LENGTH_2 = 2;
+    static const int LENGTH_3 = 3;
+    static const int LENGTH_4 = 4;
+    static const int LENGTH_8 = 8;
 
     static Map<String, String> convert = {
         "AA" : "Аа",
@@ -104,22 +104,101 @@ class UkrainianToLatin {
         "YaIa" : "Яя"
     };
 
-    static Map<String, ConvertCase> _cyrToLat;
+    static Map<String, ConvertCase> cyrToLat;
 
     static void initialize() {
         cyrToLat = new Map<String, ConvertCase>();
-/*
-        for (String item in convert) {
-            cyrToLat[item.Value.Substring(INDEX_0, LENGTH_1)] = new ConvertCase(item.Key, false);
-            cyrToLat[item.Value.Substring(INDEX_1, LENGTH_1)] = new ConvertCase(item.Key, true);
-            if (item.Key == "EE") {
-                cyrToLat["Ё"] = new ConvertCase(item.Key, false);
-                cyrToLat["ё"] = new ConvertCase(item.Key, true);
+        convert.forEach((String key, String value) {
+            cyrToLat[value.substring(INDEX_0, INDEX_1)] = new ConvertCase(key, false);
+            cyrToLat[value.substring(INDEX_1, INDEX_2)] = new ConvertCase(key, true);
+            if (key == "EE") {
+                cyrToLat["Ё"] = new ConvertCase(key, false);
+                cyrToLat["ё"] = new ConvertCase(key, true);
             }
-        }
-*/
+        });
     }
-}
 
-main() {
+    static string generateLat(String name) {
+        initialize();
+        StringBuilder result = new StringBuilder();
+        ConvertCase prevConvertCase = null;
+        for (int index = 0; index < name.length; index += 1) {
+            String curChar = name.substring(index, index + INDEX_1);
+            String nextChar = index == name.length - 1 ? null : name.substring(index + INDEX_1, index + INDEX_2);
+            if (new RegExp("[-'’,]").hasMatch(curChar)) {
+                continue;
+            }
+            if (!cyrToLat.containsKey(curChar)) {
+                if (" " == curChar) {
+                    prevConvertCase = null;
+                    result.append(' ');
+                }
+                continue;
+            }
+            ConvertCase convertCase = cyrToLat[curChar];
+            if (prevConvertCase == null) {
+                checkFirstChar(result, convertCase, nextChar != null && cyrToLat.containsKey(nextChar) ? cyrToLat[nextChar] : convertCase);
+            } else {
+                checkMiddleChar(result, convertCase, nextChar != null && cyrToLat.containsKey(nextChar) ? cyrToLat[nextChar] : convertCase);
+            }
+            prevConvertCase = convertCase;
+        }
+        return result.toString();
+    }
+
+    /**
+     * @param result
+     * @param convertCase
+     * @param nextConvertCase
+     */
+    static void checkFirstChar(StringBuilder result, ConvertCase convertCase, ConvertCase nextConvertCase) {
+        String latName = convertCase.getConvert();
+        switch (latName.length) {
+        case LENGTH_2:
+            result.append(convertCase.isLowcase() ? latName.substring(INDEX_0, INDEX_1).toLowerCase() : nextConvertCase
+                    .isLowcase() ? latName.substring(INDEX_0, INDEX_1) : latName.substring(INDEX_0, INDEX_1).toUpperCase());
+            if (convertCase.getConvert() == "ZZ" && nextConvertCase.getConvert() == "HH") {
+                result.append(nextConvertCase.isLowcase() ? "g" : "G");
+            }
+            break;
+        case LENGTH_3: case LENGTH_4:
+            result.append(convertCase.isLowcase() ? latName.substring(INDEX_0, INDEX_2).toLowerCase() : nextConvertCase
+                    .isLowcase() ? latName.substring(INDEX_0, INDEX_2) : latName.substring(INDEX_0, INDEX_2).toUpperCase());
+            break;
+        case LENGTH_8:
+            result.append(convertCase.isLowcase() ? latName.substring(INDEX_0, INDEX_4).toLowerCase() : nextConvertCase
+                    .isLowcase() ? latName.substring(INDEX_0, INDEX_4) : latName.substring(INDEX_0, INDEX_4).toUpperCase());
+            break;
+        }
+    }
+
+    /**
+     * @param result
+     * @param convertCase
+     * @param nextConvertCase
+     */
+    static void checkMiddleChar(StringBuilder result, ConvertCase convertCase, ConvertCase nextConvertCase) {
+        String latName = convertCase.getConvert();
+        switch (latName.length) {
+        case LENGTH_2:
+            result.append(convertCase.isLowcase() ? latName.substring(INDEX_1, INDEX_2).toLowerCase() : nextConvertCase
+                    .isLowcase() ? latName.substring(INDEX_1, INDEX_2) : latName.substring(INDEX_1, INDEX_2).toUpperCase());
+            if (convertCase.getConvert() == "ZZ" && nextConvertCase.getConvert() == "HH") {
+                result.append(nextConvertCase.isLowcase() ? "g" : "G");
+            }
+            break;
+        case LENGTH_3:
+            result.append(convertCase.isLowcase() ? latName.substring(INDEX_2, INDEX_3).toLowerCase() : nextConvertCase
+                    .isLowcase() ? latName.substring(INDEX_2, INDEX_3) : latName.substring(INDEX_2, INDEX_3).toUpperCase());
+            break;
+        case LENGTH_4:
+            result.append(convertCase.isLowcase() ? latName.substring(INDEX_2, INDEX_4).toLowerCase() : nextConvertCase
+                    .isLowcase() ? latName.substring(INDEX_2, INDEX_4) : latName.substring(INDEX_2, INDEX_4).toUpperCase());
+            break;
+        case LENGTH_8:
+            result.append(convertCase.isLowcase() ? latName.substring(INDEX_4, INDEX_8).toLowerCase() : nextConvertCase
+                    .isLowcase() ? latName.substring(INDEX_4, INDEX_8) : latName.substring(INDEX_4, INDEX_8).toUpperCase());
+            break;
+        }
+    }
 }
